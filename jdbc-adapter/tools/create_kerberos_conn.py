68eae8fe0243a8cccaacab284d79a699e8ab72b3
Andre Hacker <andrephacker@gmail.com>
2017-02-02 19:51:35 +0100

    minor

--- jdbc-adapter/tools/create_kerberos_conn.py
+++ jdbc-adapter/tools/create_kerberos_conn.py
@@ -69,18 +69,22 @@ def usage():
     txt += "  -o, --outfile: append output to the given file\n"
     txt += "  -r, --replace: add 'OR REPLACE' option to 'CREATE CONNECTION' statement\n"
     txt += "\nArguments:\n"
-    txt += "  connection: CONNECTION name\n"
-    txt += "  principal : Kerberos principal\n"
-    txt += "  config    : Kerberos configuration file path\n"
-    txt += "  keytab    : Kerberos keytab path\n"
+    txt += "  connection             : CONNECTION name\n"
+    txt += "  principal              : Kerberos principal\n"
+    txt += "  config                 : Kerberos configuration file path\n"
+    txt += "  keytab                 : Kerberos keytab path\n"
     txt += "  jdbc_connection_string : JDBC connection string\n"
     txt += "\nExamples:\n"
     txt += "  python {} krb_conn user@EXAMPLE.COM /etc/krb5.conf user.keytab \\\n".format(sys.argv[0])
-    txt += "    jdbc:hive2://hive-host.example.com:10000;AuthMech=1;KrbRealm=EXAMPLE.COM;KrbHostFQDN=hive-host.example.com;KrbServiceName=hive\n"
-    txt += "    =>  CREATE CONNECTION krb_conn TO '' USER 'user@EXAMPLE.COM'\n"
+    txt += "    'jdbc:hive2://hive-host.example.com:10000;AuthMech=1;KrbRealm=EXAMPLE.COM;KrbHostFQDN=hive-host.example.com;KrbServiceName=hive'\n"
+    txt += "    =>  CREATE CONNECTION krb_conn'\n"
+    txt += "        TO 'jdbc:hive2://hive-host.example.com:10000;AuthMech=1;KrbRealm=EXAMPLE.COM;KrbHostFQDN=hive-host.example.com;KrbServiceName=hive'\n"
+    txt += "        USER 'user@EXAMPLE.COM'\n"
     txt += "        IDENTIFIED BY 'ExaAuthType=Kerberos;enp6Cg==;YWFhCg=='\n"
-    txt += "\n  python {} -r -o out.txt krb_conn user@EXAMPLE.COM /etc/krb5.conf user.keytab\n".format(sys.argv[0])
-    txt += "    =>  CREATE OR REPLACE CONNECTION krb_conn TO ''\n"
+    txt += "\n  python {} -r -o out.txt krb_conn user@EXAMPLE.COM /etc/krb5.conf user.keytab \\\n".format(sys.argv[0])
+    txt += "    'jdbc:hive2://hive-host.example.com:10000;AuthMech=1;KrbRealm=EXAMPLE.COM;KrbHostFQDN=hive-host.example.com;KrbServiceName=hive'\n"
+    txt += "    =>  CREATE OR REPLACE CONNECTION krb_conn\n"
+    txt += "        TO ''\n"
     txt += "        USER 'user@EXAMPLE.COM' IDENTIFIED BY 'ExaAuthType=Kerberos;enp6Cg==;YWFhCg==' (written to out.txt)\n"
     print(txt)
 
