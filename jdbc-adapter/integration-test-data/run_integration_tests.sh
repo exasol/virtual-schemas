d72a7f4f14d6b24331f457559270c824d35919d4
Sebastian B <redcatbear@ursus-minor.de>
2018-09-07 13:54:05 +0200

    Pmi 73 take over sybase from hendrik (#33)
    * PMI-16: Set fixed database version for CI build. Cleaned up integration test script.
    
    * PMI-16: Fixed quoting.
    
    * PMI-16: Corrected docker image version.
    
    * PMI-76: Cherry-picked integration test script improvements.

--- jdbc-adapter/integration-test-data/run_integration_tests.sh
+++ jdbc-adapter/integration-test-data/run_integration_tests.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
-
 # This script executes integration tests as defined in
 # integration-test-travis.yaml (currently only Exasol integration tests).
-
+#
 # An Exasol instance is run using the exasol/docker-db image. Therefore, a
 # working installation of Docker and sudo privileges are required.
 
 set -eux
-
 cd "$(dirname "$0")/.."
 
-config="$(pwd)/integration-test-data/integration-test-travis.yaml"
+readonly config="$(pwd)/integration-test-data/integration-test-travis.yaml"
+readonly exasol_version='6.0.10-d1'
+readonly tmp="$(pwd)/integration-test-data/exa"
 
 function cleanup() {
     docker rm -f exasoldb || true
-    sudo rm -rf integration-test-data/exa || true
+    sudo rm -rf "$tmp" || true
 }
 trap cleanup EXIT
 
 # Setup directory "exa" with pre-configured EXAConf to attach it to the exasoldb docker container
-mkdir -p integration-test-data/exa/{etc,data/storage}
+mkdir -p "$tmp"/{etc,data/storage}
 cp integration-test-data/EXAConf integration-test-data/exa/etc/EXAConf
 dd if=/dev/zero of=integration-test-data/exa/data/storage/dev.1.data bs=1 count=1 seek=4G
 touch integration-test-data/exa/data/storage/dev.1.meta
@@ -31,8 +31,8 @@ docker run \
     -p 6594:6583 \
     --detach \
     --privileged \
-    -v "$(pwd)/integration-test-data/exa:/exa" \
-    exasol/docker-db:latest \
+    -v "$tmp:/exa" \
+    "exasol/docker-db:$exasol_version" \
     init-sc --node-id 11
 
 docker logs -f exasoldb &
@@ -43,7 +43,7 @@ sleep 30
 
 mvn -q clean package
 
-# Load virtualschema-jdbc-adapter jar into BucketFS and wait until it's available.
+# Load virtualschema-jdbc-adapter JAR into BucketFS and wait until it's available.
 mvn -q pre-integration-test -DskipTests -Pit -Dintegrationtest.configfile="$config"
 (docker exec exasoldb sh -c 'tail -f -n +0 /exa/logs/cored/*bucket*' &) | \
     grep -q -i 'File.*virtualschema-jdbc-adapter.*linked'
