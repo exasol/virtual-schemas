a400ba416d10adc1448928bb2432e1c01ed74424
Sebastian Bär <sebastian.baer@exasol.com>
2018-09-14 09:21:48 +0200

    PMI-75: Refactored IntegrationTestSetup.java for better readability and to reduce static methods to a minimum.

--- jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/dialects/IntegrationTestSetup.java
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/dialects/IntegrationTestSetup.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.util.logging.Logger;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpHeaders;
@@ -15,72 +16,106 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.FileEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-
 /**
- * The main method of this class will be called in the pre-integration-test maven phase before the actual integration-test phase starst.
- * Here we have to setup the integration test environment
+ * The main method of this class will be called in the
+ * <code>pre-integration-test</code> Maven phase before the actual
+ * <code>integration-test</code> phase starts.
  *
- * Attention: This does not deploy the latest jar, because the dist maven module is cleaned and build after this module. Right now you need to do something like "mvn clean package && mvn verify -Pit -D..." to upload and test the latest jar.
+ * <p>
+ * We upload the JAR file containing the virtual schema adapter to BucketFS so
+ * that we don't accidentally forget to do this before the integration test.
+ * There is a caveat though: if you run this on a cluster, the cluster nodes
+ * will not replicate the JAR instantly. This takes a while. So in clustered
+ * environments it is safer to upload the file and wait a while.
+ * </p>
+ *
+ * <p>
+ * <strong>Attention:</strong>
+ * </p>
+ *
+ * <p>
+ * This does not deploy the latest JAR, because the distribution Maven module is
+ * and and build after this module. Right now you need to do something like
+ * <code><pre>mvn clean package && mvn verify -Pit -D...</pre></code> to upload
+ * and test the latest JAR.
+ * </p>
  */
 public class IntegrationTestSetup {
+    private static final String ARTIFACT_DISTRIBUTION_NAME = "virtualschema-jdbc-adapter-dist";
+    private static final Logger LOGGER = Logger.getLogger(IntegrationTestConfig.class.getName());
+    private final IntegrationTestConfig config;
+    private final String version;
 
-    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
-        if (args.length > 2) {
-            Boolean skipTestSetup = Boolean.valueOf(args[2]);
-            if (skipTestSetup) {
-                System.out.println("Skip setup of the integration test environment");
-                return;
-            }
+    public static void main(final String[] args) throws ClientProtocolException, IOException, URISyntaxException {
+        if (isSkippingIntegrationTestConfigured(args)) {
+            LOGGER.info("Skipping setup of the integration test environment");
+        } else {
+            LOGGER.info("Setting up the integration test environment");
+            final String projectVersion = args[0];
+            final String configFile = args[1];
+            new IntegrationTestSetup(new IntegrationTestConfig(configFile), projectVersion).run();
         }
-
-        System.out.println("Start setup of the integration test environment");
-        String projectVersion = args[0];
-        String configFile = args[1];
-
-        IntegrationTestConfig config = new IntegrationTestConfig(configFile);
-
-        String bucketFSurl = config.getBucketFSURL();  
-        String bucketFSpassword = config.getBucketFSPassword();
-        		
-        
-        // The local path look like "virtualschema-jdbc-adapter-dist/target/virtualschema-jdbc-adapter-dist-1.0.2-SNAPSHOT.jar"
-        String artifactDistName = "virtualschema-jdbc-adapter-dist";
-        
-        String jarName = artifactDistName + "-" + projectVersion + ".jar";
-        
-        String jarLocalPath = "../" + artifactDistName + "/target/" + jarName;
-     
-        
-        uploadFileToBucketFS(bucketFSurl+"/"+jarName, jarLocalPath, bucketFSpassword);
-        
-        //uploadFileToBucketFS("http://192.168.106.131:2580/bucket1/original-virtualschema-jdbc-adapter-dist-1.0.2-SNAPSHOT.jar", "C:\\Users\\tb\\Desktop\\github-repos\\virtual-schemas\\jdbc-adapter\\virtualschema-jdbc-adapter-dist\\target\\original-virtualschema-jdbc-adapter-dist-1.0.2-SNAPSHOT.jar","bucket1");
-                   
     }
 
-  
-    private static void uploadFileToBucketFS(String url, String filePath, String password) throws ClientProtocolException, IOException, URISyntaxException {
-    	
-    	HttpClient httpClient = HttpClientBuilder.create().build();
-        URIBuilder uriBuilder = new URIBuilder(url);
-        HttpPut request = new HttpPut(uriBuilder.build());
-                
-        String auth = "w:"+password;
-        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("UTF-8")));
-        String authHeader = "Basic " + new String(encodedAuth);
+    private static boolean isSkippingIntegrationTestConfigured(final String[] args) {
+        return args.length > 2 && Boolean.valueOf(args[2]);
+    }
+
+    private void run() throws ClientProtocolException, IOException, URISyntaxException {
+        uploadFileToBucketFS(getJarUrlForBucketFS(this.config.getBucketFSURL()), //
+                getLocalJarPath(), //
+                this.config.getBucketFSPassword());
+    }
+
+    private IntegrationTestSetup(final IntegrationTestConfig config, final String version) {
+        this.config = config;
+        this.version = version;
+    }
+
+    private String getJarUrlForBucketFS(final String bucketFSurl) {
+        return bucketFSurl + "/" + getJarName(this.version);
+    }
+
+    private String getLocalJarPath() {
+        return ".." + File.separator + ARTIFACT_DISTRIBUTION_NAME + File.separator + "target" + File.separator
+                + getJarName(this.version);
+    }
+
+    private String getJarName(final String projectVersion) {
+        return ARTIFACT_DISTRIBUTION_NAME + "-" + projectVersion + ".jar";
+    }
+
+    private void uploadFileToBucketFS(final String url, final String filePath, final String password)
+            throws ClientProtocolException, IOException, URISyntaxException {
+        LOGGER.info(() -> "Uploading \"" + filePath + "\"" + " to \"" + url + "\"");
+        final HttpPut request = buildPutRequest(url, filePath, password);
+        final HttpResponse response = executePutRequest(request);
+        handleResponse(response);
+        LOGGER.fine(() -> "HTTP PUT response:" + System.lineSeparator() + response);
+    }
+
+    private void handleResponse(final HttpResponse response) throws IOException {
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new IOException(response.toString());
+        }
+    }
+
+    private HttpResponse executePutRequest(final HttpPut request) throws IOException, ClientProtocolException {
+        final HttpClient httpClient = HttpClientBuilder.create().build();
+        final HttpResponse response = httpClient.execute(request);
+        return response;
+    }
+
+    private HttpPut buildPutRequest(final String url, final String filePath, final String password)
+            throws URISyntaxException {
+        final URIBuilder uriBuilder = new URIBuilder(url);
+        final HttpPut request = new HttpPut(uriBuilder.build());
+        final String auth = "w:" + password;
+        final byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("UTF-8")));
+        final String authHeader = "Basic " + new String(encodedAuth);
         request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
-         
-        FileEntity fileEntity = new FileEntity(new File(filePath));
-        
+        final FileEntity fileEntity = new FileEntity(new File(filePath));
         request.setEntity(fileEntity);
-        
-        HttpResponse response = httpClient.execute(request);
-
-        if ( response.getStatusLine().getStatusCode() != 200 ) 
-        	throw new IOException( response.toString() );
-        
-        System.out.println (response);
-        
+        return request;
     }
-    
-    
 }
