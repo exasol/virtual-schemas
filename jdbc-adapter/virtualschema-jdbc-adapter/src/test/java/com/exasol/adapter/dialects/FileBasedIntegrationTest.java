ef6007e6931edb1a8b9f79f4661219a0c6d46a27
Sebastian Nehls <sn@exasol.com>
2019-01-18 08:18:24 +0100

    Executing all testfiles in the integration test directory

--- jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/dialects/FileBasedIntegrationTest.java
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/dialects/FileBasedIntegrationTest.java
@@ -3,11 +3,14 @@ package com.exasol.adapter.dialects;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonValue;
 
 import com.exasol.ExaMetadata;
+import com.exasol.adapter.AdapterException;
 import com.exasol.adapter.dialects.impl.ExasolSqlDialect;
 import com.exasol.adapter.json.RequestJsonParser;
 import com.exasol.adapter.metadata.SchemaMetadataInfo;
@@ -21,36 +24,58 @@ import com.google.common.io.Files;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+
+
 public class FileBasedIntegrationTest {
+    private static final String INTEGRATION_TESTFILES_DIR = "target/test-classes/integration";
 
     @Test
     public void testPushdownFromTestFile() throws Exception {
-        //open file and parse pushdown
-        String file = "target/test-classes/integration/vschema_simple_inner_join.json";
-        String json = Files.toString(new File(file), Charsets.UTF_8);
+        File testDir = new File(INTEGRATION_TESTFILES_DIR);
+        //FilenameFilter filter = (dir, name) -> name.endsWith(".json");
+        File[] files = testDir.listFiles((dir, name) -> name.endsWith(".json"));
+        for (File testFile : files) {
+            String jsonTest = Files.toString(testFile, Charsets.UTF_8);
+            PushdownRequest pushdownRequest = getPushdownRequestFrom(jsonTest);
+            String pushdownQuery = generatePushdownQuery(pushdownRequest, hasMultipleTables(jsonTest));
+            String expectedPushdownQuery = getExpectedPushdownQueryFrom(jsonTest);
+            System.out.println("$$$$$$$$$$$$$$$$$$ " + testFile);
+            assertEquals(expectedPushdownQuery, pushdownQuery);
+        }
+    }
 
-        // get pushdown from testfile
-        JsonObject root = JsonHelper.getJsonObject(json);
+    private PushdownRequest getPushdownRequestFrom(String jsonTest) throws Exception {
+        JsonObject root = JsonHelper.getJsonObject(jsonTest);
         JsonObject test = root.getJsonArray("testCases").getValuesAs(JsonObject.class).get(0);
         String req = test.getJsonArray("expectedPushdownRequest").get(0).toString();
         RequestJsonParser parser = new RequestJsonParser();
         AdapterRequest request = parser.parseRequest(req);
         PushdownRequest pushdownRequest = (PushdownRequest) request;
+        return pushdownRequest;
+    }
 
-        //geenrate SQL
+    private Boolean hasMultipleTables(String jsonTest) throws Exception {
+        JsonObject root = JsonHelper.getJsonObject(jsonTest);
+        JsonObject test = root.getJsonArray("testCases").getValuesAs(JsonObject.class).get(0);
+        JsonValue req = test.getJsonArray("expectedPushdownRequest").get(0);
+        int size = ((JsonObject) req).getJsonArray("involvedTables").size();
+        System.out.println("SIZESIZESIZSE: " + size);
+        return size > 1;
+    }
+
+    private String generatePushdownQuery(PushdownRequest pushdownRequest, Boolean multipleTables) throws AdapterException {
         String schemaName = "LS";
-        SqlGenerationContext context = new SqlGenerationContext("", schemaName, false, true);
+        SqlGenerationContext context = new SqlGenerationContext("", schemaName, false, multipleTables);
         SqlDialectContext dialectContext = new SqlDialectContext(Mockito.mock(SchemaAdapterNotes.class));
         ExasolSqlDialect dialect = new ExasolSqlDialect(dialectContext);
         final SqlGenerationVisitor sqlGeneratorVisitor = dialect.getSqlGenerationVisitor(context);
-        final String pushdownQuery = pushdownRequest.getSelect().accept(sqlGeneratorVisitor);
+        return pushdownRequest.getSelect().accept(sqlGeneratorVisitor);
+    }
 
-        
-
-        // compare to expected result
-        String expectedPushdown = test.getJsonObject("expectedPushdownResponse").getJsonArray("Exasol").get(0)
+    private String getExpectedPushdownQueryFrom(String jsonTest) throws Exception {
+        JsonObject root = JsonHelper.getJsonObject(jsonTest);
+        JsonObject test = root.getJsonArray("testCases").getValuesAs(JsonObject.class).get(0);
+        return test.getJsonObject("expectedPushdownResponse").getJsonArray("Exasol").get(0)
                 .toString().replaceAll("\\\\\"", "\"").replaceAll("^\"+", "").replaceAll("\"$", "");
-
-        assertEquals(pushdownQuery, expectedPushdown);
     }
 }
