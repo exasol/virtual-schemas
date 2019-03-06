081e9c091825b2fdf52a5dd649fd0d96d480c319
Sebastian Nehls <sn@exasol.com>
2019-03-06 10:26:28 +0100

    #93 added postgresql identifier handling to SQLDialectContext

---
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/main/java/com/exasol/adapter/dialects/PostgreSQLIdentifierMapping.java
@@ -0,0 +1,5 @@
+package com.exasol.adapter.dialects;
+
+public enum PostgreSQLIdentifierMapping {
+    POSTGRESQL_IDENTIFIER_MAPPING_CONVERT_TO_UPPER, POSTGRESQL_IDENTIFER_MAPPING_PRESERVE_ORIGINAL_CASE
+}
