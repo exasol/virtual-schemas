951cabbb2b9f524482e73648c51e16c51f169cc7
Sebastian Bär <sebastian.baer@exasol.com>
2019-04-15 12:14:55 +0200

    #131: Added missing serial IDs.

--- jdbc-adapter/virtualschema-jdbc-adapter/src/main/java/com/exasol/adapter/dialects/SqlDialectFactoryException.java
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/main/java/com/exasol/adapter/dialects/SqlDialectFactoryException.java
@@ -4,6 +4,8 @@ package com.exasol.adapter.dialects;
  * This class represents exceptional conditions that occur during creation of SQL dialects.
  */
 public class SqlDialectFactoryException extends RuntimeException {
+    private static final long serialVersionUID = 5317697617684968131L;
+
     /**
      * Create a new {@link SqlDialectFactoryException}
      *
--- jdbc-adapter/virtualschema-jdbc-adapter/src/main/java/com/exasol/adapter/dialects/SqlGenerationVisitorException.java
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/main/java/com/exasol/adapter/dialects/SqlGenerationVisitorException.java
@@ -1,7 +1,9 @@
 package com.exasol.adapter.dialects;
 
 public class SqlGenerationVisitorException extends RuntimeException {
+    private static final long serialVersionUID = -3119998339625992764L;
+
     public SqlGenerationVisitorException(final String message, final Throwable cause) {
-        super(message,cause);
+        super(message, cause);
     }
 }
