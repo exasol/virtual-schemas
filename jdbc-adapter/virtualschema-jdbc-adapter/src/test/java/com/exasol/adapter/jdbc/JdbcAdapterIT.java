d947f2c7734b7d8b772869b19a701966aab20969
Sebastian Bär <redcatbear@ursus-minor.de>
2019-05-03 09:48:28 +0200

    Enhancement/162 automatically register sql dialect adapters (#165)
    * #45: Rewrote parts of the JDBC adapter to use the new `common` code.
    * #121: Used new request dispatching from `common`
    * #123: Improved how schemas identify support for structural elements
    * #131: Refactored metadata reading
    * #155: moved property validation logic to dialects
    * #162: Automatically register SQL dialects.

---
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/test/java/com/exasol/adapter/jdbc/JdbcAdapterIT.java
@@ -0,0 +1,37 @@
+package com.exasol.adapter.jdbc;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.exasol.adapter.*;
+import com.exasol.adapter.response.GetCapabilitiesResponse;
+
+class JdbcAdapterIT {
+    @Test
+    public void testRegisteredDialects() throws AdapterException {
+        final String rawRequest = "{\n" //
+                + "    \"type\" : \"getCapabilities\",\n" //
+                + "    \"schemaMetadataInfo\" :\n" //
+                + "    {\n" //
+                + "        \"name\" : \"foo\",\n" //
+                + "        \"properties\" :\n" //
+                + "        {\n" //
+                + "            \"SQL_DIALECT\" : \"EXASOL\"\n" //
+                + "        }\n" //
+                + "    }\n" //
+                + "}";
+        final VirtualSchemaAdapter adapterMock = Mockito.mock(VirtualSchemaAdapter.class);
+        when(adapterMock.getCapabilities(any(), any())).thenReturn(GetCapabilitiesResponse.builder().build());
+        RequestDispatcher.adapterCall(null, rawRequest);
+        final List<AdapterFactory> registeredFactories = AdapterRegistry.getInstance().getRegisteredAdapterFactories();
+        assertThat(registeredFactories, hasItem(instanceOf(JdbcAdapterFactory.class)));
+    }
+}
