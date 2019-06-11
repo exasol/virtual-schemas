e03748cf8e0d4ce4596fedb2bdaddef26068d5d9
Anastasiia Sergienko <46891819+AnastasiiaSergienko@users.noreply.github.com>
2019-06-11 14:28:20 +0200

    #201: fixed 'order by' clause problems (#202)
    * #201: Fixed 'ORDER BY' clause problems

---
+++ jdbc-adapter/virtualschema-jdbc-adapter/src/main/java/com/exasol/adapter/dialects/bigquery/BigQuerySqlGenerationVisitor.java
@@ -0,0 +1,33 @@
+package com.exasol.adapter.dialects.bigquery;
+
+import com.exasol.adapter.*;
+import com.exasol.adapter.dialects.*;
+import com.exasol.adapter.sql.*;
+
+import java.util.*;
+
+/**
+ * This class implements a Google-Big-Query-specific variant of a SQL generation visitor.
+ */
+public class BigQuerySqlGenerationVisitor extends SqlGenerationVisitor {
+    /**
+     * Create a new instance of {@link BigQuerySqlGenerationVisitor}.
+     * 
+     * @param dialect SQL dialect
+     * @param context SQL generation context
+     */
+    public BigQuerySqlGenerationVisitor(final SqlDialect dialect, final SqlGenerationContext context) {
+        super(dialect, context);
+    }
+
+    @Override
+    public String visit(final SqlOrderBy orderBy) throws AdapterException {
+        final List<String> sqlOrderElement = new ArrayList<>();
+        for (int i = 0; i < orderBy.getExpressions().size(); ++i) {
+            final boolean isAscending = orderBy.isAscending().get(i);
+            final String elementSql = orderBy.getExpressions().get(i).accept(this) + (isAscending ? "" : " DESC");
+            sqlOrderElement.add(elementSql);
+        }
+        return "ORDER BY " + String.join(", ", sqlOrderElement);
+    }
+}
