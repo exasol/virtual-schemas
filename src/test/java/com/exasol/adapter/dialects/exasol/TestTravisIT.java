7afda3a3ac21793f8e21df9c2b884c0ce0317995
anastasiiasergienko <anastasiia.sergienko@exasol.com>
2020-01-15 11:48:00 +0100

    #223: addedTravisContainerTest

--- src/test/java/com/exasol/adapter/dialects/exasol/TestTravisIT.java
+++ src/test/java/com/exasol/adapter/dialects/exasol/TestTravisIT.java
@@ -1,37 +1,30 @@
 package com.exasol.adapter.dialects.exasol;
 
-import com.exasol.bucketfs.Bucket;
-import com.exasol.bucketfs.BucketAccessException;
-import com.exasol.containers.ExasolContainer;
-import com.exasol.containers.ExasolContainerConstants;
-import org.junit.jupiter.api.*;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.nio.file.Path;
-import java.sql.*;
-import java.util.concurrent.TimeUnit;
+import com.exasol.containers.ExasolContainer;
+import com.exasol.containers.ExasolContainerConstants;
 
 @Tag("integration")
 @Testcontainers
 public class TestTravisIT {
-    private static final String ROW_LEVEL_SECURITY_JAR_NAME_AND_VERSION = "row-level-security-dist-0.2.0.jar";
-    private static final String VIRTUAL_SCHEMA_RLS_JDBC_NAME = "VIRTUAL_SCHEMA_RLS_JDBC";
-    private static final String VIRTUAL_SCHEMA_RLS_JDBC_LOCAL_NAME = "VIRTUAL_SCHEMA_RLS_JDBC_LOCAL";
-    private static final String VIRTUAL_SCHEMA_RLS_EXA_NAME = "VIRTUAL_SCHEMA_RLS_EXA";
-    private static final String VIRTUAL_SCHEMA_RLS_EXA_LOCAL_NAME = "VIRTUAL_SCHEMA_RLS_EXA_LOCAL";
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestTravisIT.class);
     @Container
     private static final ExasolContainer<? extends ExasolContainer<?>> container = new ExasolContainer<>(
-            ExasolContainerConstants.EXASOL_DOCKER_IMAGE_REFERENCE);
-    public static final String RLS_SALES_UNPROTECTED = "RLS_SALES_UNPROTECTED";
-    public static final String RLS_SALES_TENANTS = "RLS_SALES_TENANTS";
-    public static final String RLS_SALES_ROLES = "RLS_SALES_ROLES";
-    private static Statement statement;
-
+            ExasolContainerConstants.EXASOL_DOCKER_IMAGE_REFERENCE) //
+                    .withLogConsumer(new Slf4jLogConsumer(LOGGER));
 
     @Test
     void testExaRlsUsersTableIsFilteredOut() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(180);
+        TimeUnit.SECONDS.sleep(20);
         System.out.println("Reaches test.");
     }
 }
